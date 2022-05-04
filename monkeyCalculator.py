fcfbf6b43e93c3655c80e7a30553436557d3790e
Ashton Bravo <Ashton.Bravo@vu.com>
2022-05-03 23:28:51 -0500

    test

--- monkeyCalculator.py
+++ monkeyCalculator.py
@@ -1,8 +1,17 @@
-def calculateTime():
+#def calculateTime():
     
     # This first line is provided for you
-    monkey_one = input("Is the first monkey smiling?:  ")
-    monkey_two = input("Is the second monkey smiling?: ")
+monkey_one = input("Is the first monkey smiling? :  ")
+monkey_two = input("Is the second monkey smiling? : ")
+
+if monkey_one =="No" or "no" and monkey_two == "No" or "no":
+    print("Uh on! We're in trouble!")
+if monkey_one == "Yes" or "yes" and monkey_two == "No" or "no":
+    print("Yay! We're going to have a good day!")
+if monkey_one == "No" or "no" and monkey_two == "Yes" or "yes":
+    print("Yay! We're going to have a good day!")
+
+
 
     # end assignment
 
